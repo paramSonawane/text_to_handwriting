abc7a504d4ae5d3ad6db192463291f5b14875a0c
paramSonawane <paramsonawane@gmail.com>
2021-05-29 20:54:56 +0530

    textToHandwriting

---
+++ index.js
@@ -0,0 +1,506 @@
+function preload(){
+    Caps = [
+        [
+            loadImage('data/A/A_1.png'),
+            loadImage('data/A/A_2.png'),
+            loadImage('data/A/A_3.png'),
+        ],
+        [
+            loadImage('data/B/B_1.png'),
+            loadImage('data/B/B_2.png'),
+            loadImage('data/B/B_3.png'),
+        ],
+        [
+            loadImage('data/C/C_1.png'),
+            loadImage('data/C/C_2.png'),
+            loadImage('data/C/C_3.png'),
+        ],
+        [
+            loadImage('data/D/D_1.png'),
+            loadImage('data/D/D_2.png'),
+            loadImage('data/D/D_3.png'),
+        ],
+        [
+            loadImage('data/E/E_1.png'),
+            loadImage('data/E/E_2.png'),
+            loadImage('data/E/E_3.png'),
+        ],
+        [
+            loadImage('data/F/F_1.png'),
+            loadImage('data/F/F_2.png'),
+            loadImage('data/F/F_3.png'),
+        ],
+        [
+            loadImage('data/G/G_1.png'),
+            loadImage('data/G/G_2.png'),
+            loadImage('data/G/G_3.png'),
+        ],
+        [
+            loadImage('data/H/H_1.png'),
+            loadImage('data/H/H_2.png'),
+            loadImage('data/H/H_3.png'),
+        ],
+        [
+            loadImage('data/I/I_1.png'),
+            loadImage('data/I/I_2.png'),
+            loadImage('data/I/I_3.png'),
+        ],
+        [
+            loadImage('data/J/J_1.png'),
+            loadImage('data/J/J_2.png'),
+            loadImage('data/J/J_3.png'),
+        ],
+        [
+            loadImage('data/K/K_1.png'),
+            loadImage('data/K/K_2.png'),
+            loadImage('data/K/K_3.png'),
+        ],
+        [
+            loadImage('data/L/L_1.png'),
+            loadImage('data/L/L_2.png'),
+            loadImage('data/L/L_3.png'),
+        ],
+        [
+            loadImage('data/M/M_1.png'),
+            loadImage('data/M/M_2.png'),
+            loadImage('data/M/M_3.png'),
+        ],
+        [
+            loadImage('data/N/N_1.png'),
+            loadImage('data/N/N_2.png'),
+            loadImage('data/N/N_3.png'),
+        ],
+        [
+            loadImage('data/O/O_1.png'),
+            loadImage('data/O/O_2.png'),
+            loadImage('data/O/O_3.png'),
+        ],
+        [
+            loadImage('data/P/P_1.png'),
+            loadImage('data/P/P_2.png'),
+            loadImage('data/P/P_3.png'),
+        ],
+        [
+            loadImage('data/Q/Q_1.png'),
+            loadImage('data/Q/Q_2.png'),
+            loadImage('data/Q/Q_3.png'),
+        ],
+        [
+            loadImage('data/R/R_1.png'),
+            loadImage('data/R/R_2.png'),
+            loadImage('data/R/R_3.png'),
+        ],
+        [
+            loadImage('data/S/S_1.png'),
+            loadImage('data/S/S_2.png'),
+            loadImage('data/S/S_3.png'),
+        ],
+        [
+            loadImage('data/T/T_1.png'),
+            loadImage('data/T/T_2.png'),
+            loadImage('data/T/T_3.png'),
+        ],
+        [
+            loadImage('data/U/U_1.png'),
+            loadImage('data/U/U_2.png'),
+            loadImage('data/U/U_3.png'),
+        ],
+        [
+            loadImage('data/V/V_1.png'),
+            loadImage('data/V/V_2.png'),
+            loadImage('data/V/V_3.png'),
+        ],
+        [
+            loadImage('data/W/W_1.png'),
+            loadImage('data/W/W_2.png'),
+            loadImage('data/W/W_3.png'),
+        ],
+        [
+            loadImage('data/X/X_1.png'),
+            loadImage('data/X/X_2.png'),
+            loadImage('data/X/X_3.png'),
+        ],
+        [
+            loadImage('data/Y/Y_1.png'),
+            loadImage('data/Y/Y_2.png'),
+            loadImage('data/Y/Y_3.png'),
+        ],
+        [
+            loadImage('data/Z/Z_1.png'),
+            loadImage('data/Z/Z_2.png'),
+            loadImage('data/Z/Z_3.png'),
+        ],
+    ]
+
+    smalls = [
+        [
+            loadImage('data/a/a_1.png'),
+            loadImage('data/a/a_2.png'),
+            loadImage('data/a/a_3.png'),
+        ],
+        [
+            loadImage('data/b/b_1.png'),
+            loadImage('data/b/b_2.png'),
+            loadImage('data/b/b_3.png'),
+        ],
+        [
+            loadImage('data/c/c_1.png'),
+            loadImage('data/c/c_2.png'),
+            loadImage('data/c/c_3.png'),
+        ],
+        [
+            loadImage('data/d/d_1.png'),
+            loadImage('data/d/d_2.png'),
+            loadImage('data/d/d_3.png'),
+        ],
+        [
+            loadImage('data/e/e_1.png'),
+            loadImage('data/e/e_2.png'),
+            loadImage('data/e/e_3.png'),
+        ],
+        [
+            loadImage('data/f/f_1.png'),
+            loadImage('data/f/f_2.png'),
+            loadImage('data/f/f_3.png'),
+        ],
+        [
+            loadImage('data/g/g_1.png'),
+            loadImage('data/g/g_2.png'),
+            loadImage('data/g/g_3.png'),
+        ],
+        [
+            loadImage('data/h/h_1.png'),
+            loadImage('data/h/h_2.png'),
+            loadImage('data/h/h_3.png'),
+        ],
+        [
+            loadImage('data/i/i_1.png'),
+            loadImage('data/i/i_2.png'),
+            loadImage('data/i/i_3.png'),
+        ],
+        [
+            loadImage('data/j/j_1.png'),
+            loadImage('data/j/j_2.png'),
+            loadImage('data/j/j_3.png'),
+        ],
+        [
+            loadImage('data/k/k_1.png'),
+            loadImage('data/k/k_2.png'),
+            loadImage('data/k/k_3.png'),
+        ],
+        [
+            loadImage('data/l/l_1.png'),
+            loadImage('data/l/l_2.png'),
+            loadImage('data/l/l_3.png'),
+        ],
+        [
+            loadImage('data/m/m_1.png'),
+            loadImage('data/m/m_2.png'),
+            loadImage('data/m/m_3.png'),
+        ],
+        [
+            loadImage('data/n/n_1.png'),
+            loadImage('data/n/n_2.png'),
+            loadImage('data/n/n_3.png'),
+        ],
+        [
+            loadImage('data/o/o_1.png'),
+            loadImage('data/o/o_2.png'),
+            loadImage('data/o/o_3.png'),
+        ],
+        [
+            loadImage('data/p/p_1.png'),
+            loadImage('data/p/p_2.png'),
+            loadImage('data/p/p_3.png'),
+        ],
+        [
+            loadImage('data/q/q_1.png'),
+            loadImage('data/q/q_2.png'),
+            loadImage('data/q/q_3.png'),
+        ],
+        [
+            loadImage('data/r/r_1.png'),
+            loadImage('data/r/r_2.png'),
+            loadImage('data/r/r_3.png'),
+        ],
+        [
+            loadImage('data/s/s_1.png'),
+            loadImage('data/s/s_2.png'),
+            loadImage('data/s/s_3.png'),
+        ],
+        [
+            loadImage('data/t/t_1.png'),
+            loadImage('data/t/t_2.png'),
+            loadImage('data/t/t_3.png'),
+        ],
+        [
+            loadImage('data/u/u_1.png'),
+            loadImage('data/u/u_2.png'),
+            loadImage('data/u/u_3.png'),
+        ],
+        [
+            loadImage('data/v/v_1.png'),
+            loadImage('data/v/v_2.png'),
+            loadImage('data/v/v_3.png'),
+        ],
+        [
+            loadImage('data/w/w_1.png'),
+            loadImage('data/w/w_2.png'),
+            loadImage('data/w/w_3.png'),
+        ],
+        [
+            loadImage('data/x/x_1.png'),
+            loadImage('data/x/x_2.png'),
+            loadImage('data/x/x_3.png'),
+        ],
+        [
+            loadImage('data/y/y_1.png'),
+            loadImage('data/y/y_2.png'),
+            loadImage('data/y/y_3.png'),
+        ],
+        [
+            loadImage('data/z/z_1.png'),
+            loadImage('data/z/z_2.png'),
+            loadImage('data/z/z_3.png'),
+        ],
+    ]
+
+    nums = [
+        [
+            loadImage('data/0/0_1.png'),
+            loadImage('data/0/0_2.png'),
+            loadImage('data/0/0_3.png'),
+        ],
+        [
+            loadImage('data/1/1_1.png'),
+            loadImage('data/1/1_2.png'),
+            loadImage('data/1/1_3.png'),
+        ],
+        [
+            loadImage('data/2/2_1.png'),
+            loadImage('data/2/2_2.png'),
+            loadImage('data/2/2_3.png'),
+        ],
+        [
+            loadImage('data/3/3_1.png'),
+            loadImage('data/3/3_2.png'),
+            loadImage('data/3/3_3.png'),
+        ],
+        [
+            loadImage('data/4/4_1.png'),
+            loadImage('data/4/4_2.png'),
+            loadImage('data/4/4_3.png'),
+        ],
+        [
+            loadImage('data/5/5_1.png'),
+            loadImage('data/5/5_2.png'),
+            loadImage('data/5/5_3.png'),
+        ],
+        [
+            loadImage('data/6/6_1.png'),
+            loadImage('data/6/6_2.png'),
+            loadImage('data/6/6_3.png'),
+        ],
+        [
+            loadImage('data/7/7_1.png'),
+            loadImage('data/7/7_2.png'),
+            loadImage('data/7/7_3.png'),
+        ],
+        [
+            loadImage('data/8/8_1.png'),
+            loadImage('data/8/8_2.png'),
+            loadImage('data/8/8_3.png'),
+        ],
+        [
+            loadImage('data/9/9_1.png'),
+            loadImage('data/9/9_2.png'),
+            loadImage('data/9/9_3.png'),
+        ],
+    ]
+
+    comma = [
+        loadImage('data/comma/comma_1.png'),
+        loadImage('data/comma/comma_2.png'),
+        loadImage('data/comma/comma_3.png'),
+    ]
+
+    colon = [
+        loadImage('data/colon/colon_1.png'),
+        loadImage('data/colon/colon_2.png'),
+        loadImage('data/colon/colon_3.png'),
+    ]
+
+    dot = [
+        loadImage('data/dot/dot_1.png'),
+        loadImage('data/dot/dot_2.png'),
+        loadImage('data/dot/dot_3.png'),
+    ]
+
+    equals = [
+        loadImage('data/equals/equals_1.png'),
+        loadImage('data/equals/equals_2.png'),
+        loadImage('data/equals/equals_3.png'),
+    ]
+
+    rndBrcC = [
+        loadImage('data/rndBrcC/rndBrcC_1.png'),
+        loadImage('data/rndBrcC/rndBrcC_2.png'),
+        loadImage('data/rndBrcC/rndBrcC_3.png'),
+    ]
+
+    rndBrcO = [
+        loadImage('data/rndBrcO/rndBrcO_1.png'),
+        loadImage('data/rndBrcO/rndBrcO_2.png'),
+        loadImage('data/rndBrcO/rndBrcO_3.png'),
+    ]
+
+    sqrBrcC = [
+        loadImage('data/sqrBrcC/sqrBrcC_1.png'),
+        loadImage('data/sqrBrcC/sqrBrcC_2.png'),
+        loadImage('data/sqrBrcC/sqrBrcC_3.png'),
+    ]
+
+    sqrBrcO = [
+        loadImage('data/sqrBrcO/sqrBrcO_1.png'),
+        loadImage('data/sqrBrcO/sqrBrcO_2.png'),
+        loadImage('data/sqrBrcO/sqrBrcO_3.png'),
+    ]
+
+    bg = loadImage('bg.jpg')
+}
+
+function setup() {
+    canvY = (bg.height*0.17), canvX = (bg.width*0.17)
+
+    createCanvas(canvX, canvY);
+}
+
+function draw() {
+
+}
+function myInputEvent() {
+    console.log("called")
+    background(20);
+
+    image(bg, 0, 0, canvX, canvY, 0, 0, bg.width, bg.height)
+
+    let startX = 78, startY = 148, maxHeight = 0, charSpacing = 4, wordSpacing=15;
+    X = startX
+    Y = startY
+    let lineHt = 35.5
+    let fontSize = 0.3;
+
+    inputTxt = document.getElementById('TXTAREA').value
+
+    for(let i =0; i< inputTxt.length; i++){
+        num = (inputTxt.charCodeAt(i))
+        if(inputTxt[i] == ' '){
+            X+=wordSpacing
+        }else if(inputTxt[i] == '\n'){
+            X = startX
+            Y+=lineHt
+
+        }else if(inputTxt[i] == ','){
+            img = comma[Math.floor(Math.random()*3)]
+            imgX = X
+            imgY = Y-(img.height*fontSize)
+            imgHt = img.height*fontSize;
+            imgWt = img.width*fontSize;
+            image(img, imgX, imgY, imgWt, imgHt, 0, 0, img.width, img.height)
+            X += imgWt+charSpacing;
+        }else if(inputTxt[i] == ':'){
+            img = colon[Math.floor(Math.random()*3)]
+            imgX = X
+            imgY = Y-(img.height*fontSize)
+            imgHt = img.height*fontSize;
+            imgWt = img.width*fontSize;
+            image(img, imgX, imgY, imgWt, imgHt, 0, 0, img.width, img.height)
+            X += imgWt+charSpacing;
+        }else if(inputTxt[i] == '.'){
+            img = dot[Math.floor(Math.random()*3)]
+            imgX = X
+            imgY = Y-(img.height*fontSize)
+            imgHt = img.height*fontSize;
+            imgWt = img.width*fontSize;
+            image(img, imgX, imgY, imgWt, imgHt, 0, 0, img.width, img.height)
+            X += imgWt+charSpacing;
+
+        }else if(inputTxt[i] == '='){
+            img = equals[Math.floor(Math.random()*3)]
+            imgX = X
+            imgY = Y-(img.height*fontSize)
+            imgHt = img.height*fontSize;
+            imgWt = img.width*fontSize;
+            image(img, imgX, imgY, imgWt, imgHt, 0, 0, img.width, img.height)
+            X += imgWt+charSpacing;
+        }else if(inputTxt[i] == ')'){
+            img = rndBrcC[Math.floor(Math.random()*3)]
+            imgX = X
+            imgY = Y-(img.height*fontSize)
+            imgHt = img.height*fontSize;
+            imgWt = img.width*fontSize;
+            image(img, imgX, imgY, imgWt, imgHt, 0, 0, img.width, img.height)
+            X += imgWt+charSpacing;
+        }else if(inputTxt[i] == '('){
+            img = rndBrcO[Math.floor(Math.random()*3)]
+            imgX = X
+            imgY = Y-(img.height*fontSize)
+            imgHt = img.height*fontSize;
+            imgWt = img.width*fontSize;
+            image(img, imgX, imgY, imgWt, imgHt, 0, 0, img.width, img.height)
+            X += imgWt+charSpacing;
+        }else if(inputTxt[i] == ']'){
+            img = sqrBrcC[Math.floor(Math.random()*3)]
+            imgX = X
+            imgY = Y-(img.height*fontSize)
+            imgHt = img.height*fontSize;
+            imgWt = img.width*fontSize;
+            image(img, imgX, imgY, imgWt, imgHt, 0, 0, img.width, img.height)
+            X += imgWt+charSpacing;
+        }else if(inputTxt[i] == '['){
+            img = sqrBrcO[Math.floor(Math.random()*3)]
+            imgX = X
+            imgY = Y-(img.height*fontSize)
+            imgHt = img.height*fontSize;
+            imgWt = img.width*fontSize;
+            image(img, imgX, imgY, imgWt, imgHt, 0, 0, img.width, img.height)
+            X += imgWt+charSpacing;
+        }else if(num >= 48 && num <= 57){
+            img = nums[num-48][Math.floor(Math.random()*3)]
+            imgX = X
+            imgY = Y-(img.height*fontSize)
+            imgHt = img.height*fontSize;
+            imgWt = img.width*fontSize;
+            image(img, imgX, imgY, imgWt, imgHt, 0, 0, img.width, img.height)
+            X += imgWt+charSpacing;
+        }else if(inputTxt[i] == 'g' || inputTxt[i] == 'j' ||
+                inputTxt[i] == 'p' || inputTxt[i] == 'q' ||
+                inputTxt[i] == 'y'){
+
+            img = smalls[num-97][Math.floor(Math.random()*3)]
+            imgX = X
+            imgY = Y-(img.height*fontSize)/2
+            imgHt = img.height*fontSize;
+            imgWt = img.width*fontSize;
+            image(img, imgX, imgY, imgWt, imgHt, 0, 0, img.width, img.height)
+            X += imgWt+charSpacing;
+
+        }else if(num >= 97 && num <=122){
+            img = smalls[num-97][Math.floor(Math.random()*3)]
+            imgX = X
+            imgY = Y-(img.height*fontSize)
+            imgHt = img.height*fontSize;
+            imgWt = img.width*fontSize;
+            image(img, imgX, imgY, imgWt, imgHt, 0, 0, img.width, img.height)
+            X += imgWt+charSpacing;
+        }else if(num >= 65 && num <= 90){
+            img = Caps[num-65][Math.floor(Math.random()*3)]
+            imgX = X
+            imgY = Y-(img.height*fontSize)
+            imgHt = img.height*fontSize;
+            imgWt = img.width*fontSize;
+            image(img, imgX, imgY, imgWt, imgHt, 0, 0, img.width, img.height)
+            X += imgWt+charSpacing;
+        }
+    }
+}
